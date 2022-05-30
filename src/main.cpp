825347dc9faa46c3de762bd33d5cecab64f06f71
Anna Ochab-Marcinek <anna.ochab.marcinek@gmail.com>
2022-05-30 12:58:57 +0200

    Clean code

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,3 @@
-// https://www.gnu.org/software/gsl/doc/html/interp.html#c.gsl_interp2d_eval
-// 2D Interpolation Example programs
-// my_gsl_interp2d_typehe following example performs bilinear interpolation on the unit square,
-// using z values of (0,1,0.5,1) going clockwise around the square.
-
 #include "include/interpolation2d_example.h"
 #include "include/parse_args.h"
 
